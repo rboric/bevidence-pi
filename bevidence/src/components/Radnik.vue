afb907caf0ef11f4f58cac39d88d5e12c42e8cff
Rene Borić <rboric1901@gmail.com>
2022-03-05 14:18:06 +0100

    Radnik komponenta

---
+++ bevidence/src/components/Radnik.vue
@@ -0,0 +1,17 @@
+<template>
+  <tr>
+    <td>
+      <img class="radnici-img" src="@/assets/p_p.png" alt="" />
+    </td>
+    <td>Mark</td>
+    <td>Otto</td>
+    <td>Zagreb</td>
+    <td><a href="">UREDI</a></td>
+  </tr>
+</template>
+
+<script>
+export default {
+  name: "Radnik",
+};
+</script>
